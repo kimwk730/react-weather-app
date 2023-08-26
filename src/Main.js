84a34982d1cae5d3bdb40b24f658a94304de4ae3
kimwk730 <kimwk730@users.noreply.github.com>
2023-08-26 02:36:42 -1000

    Date function created

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+
 import axios from "axios";
-import "./Main.css";
 
 export default function Main(props) {
 	const [city, setCity] = useState(props.defaultCity);
@@ -26,33 +26,24 @@ export default function Main(props) {
 		setWeatherData({
 			ready: true,
 			city: response.data.name,
-			temp: response.data.main.temp,
+			temp: Math.round(response.data.main.temp),
 			description: response.data.weather[0].description,
 			humidity: response.data.main.humidity,
-			wind: response.data.wind.speed,
+			wind: Math.round(response.data.wind.speed),
+			coordinates: response.data.coord,
+			date: new Date(response.data.dt * 1000),
+			icon: response.data.weather[0].icon,
 		});
 	}
 	if (weatherData.ready) {
 		return (
 			<div className="main">
 				<form onClick={handleSubmit}>
+					<input button="submit" value="current location" />
 					<input type="text" placeholder="Change City" onChange={updateCity} />
-					<input type="submit" value="Go" />
-					<input type="submit" value="Current" />
+					<input button="submit" value="Search" />
 				</form>
-				<h1>{city}</h1>
-				<div className="row">
-					<div className="col">
-						<WeatherInfo data={weatherData} />
-					</div>
-					<div className="col">
-						<ul>
-							<li>Time</li>
-							<li>date</li>
-							<li>day</li>
-						</ul>
-					</div>
-				</div>
+				<WeatherInfo data={weatherData} />
 			</div>
 		);
 	} else {
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Date from "./Date";
 
 export default function WeatherInfo(props) {
 	return (
-		<ul>
-			<li>{props.data.temp}</li>
-			<li>{props.data.description}</li>
-			<li>{props.data.humidity}</li>
-			<li>{props.data.wind}</li>
-		</ul>
+		<div className="weatherInfo">
+			<h1>{props.data.city}</h1>
+			<h2>{props.data.temp} °C</h2>
+			<ul>
+				<li>{props.data.description}</li>
+				<li>{props.data.humidity}%</li>
+				<li>{props.data.wind} Km/h</li>
+			</ul>
+			<Date date={props.data.date} />
+		</div>
 	);
 }
